a9eb746bc30451eaa09535affa747a471b77649a
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2019-01-14 06:12:35 +0100

    rewrite again

--- script.js
+++ script.js
@@ -144,22 +144,6 @@ function whileLessThanHundred() {
   }
 }
 
-switch (browser) {
-  case 'Edge':
-    alert( "You've got the Edge!" );
-    break;
-
-  case 'Chrome':
-  case 'Firefox':
-  case 'Safari':
-  case 'Opera':
-    alert( 'Okay we support these browsers too' );
-    break;
-
-  default:
-    alert( 'We hope that this page looks ok!' );
-}
-
 function switchIntoIfElse(){
   let browser = prompt("What browser are you using?");
   if (browser == "Edge"){
