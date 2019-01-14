5b18d7803dda328c8e8181380e25dc5ab785cca8
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2019-01-14 05:12:41 +0100

    Add funciton to log even numbers between 2 and 10

--- script.js
+++ script.js
@@ -120,3 +120,11 @@ function deepEqual(a, b) {
     return false;
   }
 }
+
+// Output even numbers between 2 and 10
+
+for(let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
